26a7fa415c8c48d42b31efc82a28e85426f5f6f0
Folka2134 <folka2134@gmail.com>
2023-11-02 14:39:44 +0000

    Implemented country filter functionality along with drop down

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Image from "next/image";
+import React from "react";
 import { useEffect, useState } from "react";
 
 interface Country {
@@ -40,9 +42,17 @@ interface Country {
 
 export default function Home() {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [uniqueRegions, setUniqueRegions] = useState<string[]>([]);
+  const [selectedRegion, setSelectedRegion] = useState("all");
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
+  const filteredCountries = countries.filter(
+    (country) => selectedRegion === "all" || country.region === selectedRegion,
+  );
+
+  // API call for countries data
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -70,16 +80,49 @@ export default function Home() {
     fetchData();
   }, []);
 
+  // Grab unique regions from countries
+  useEffect(() => {
+    const regionsSet = new Set(countries.map((country) => country.region));
+    const regionsArray = Array.from(regionsSet);
+    setUniqueRegions(regionsArray);
+  }, [countries]);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading countries: {error.message}</p>;
 
-  console.log(countries);
+  console.log(selectedRegion);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="mb-5 text-xl font-bold">Folka-Countries</h1>
+      <h1 className="mb-5 flex-1 text-xl font-bold">Folka-Countries</h1>
+      <div className="mb-5 flex w-full justify-end">
+        <div className="">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild className="w-44">
+              <Button variant="outline">
+                {selectedRegion === "all" ? "Filter by Region" : selectedRegion}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-44">
+              <DropdownMenuRadioGroup
+                value={selectedRegion}
+                onValueChange={setSelectedRegion}
+              >
+                <DropdownMenuRadioItem key="all" value="all">
+                  All Regions
+                </DropdownMenuRadioItem>
+                {uniqueRegions.map((region) => (
+                  <DropdownMenuRadioItem key={region} value={region}>
+                    {region}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
       <div className="grid grid-cols-5 gap-3">
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <Card key={country.name.common} className="">
             <CardHeader>
               <Image
