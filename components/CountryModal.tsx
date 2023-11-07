b741a6608a6e374c70f4e9b0ec2a0ba5c72cdf22
Folka2134 <folka2134@gmail.com>
2023-11-07 11:51:29 +0000

    fixed card error

--- components/CountryModal.tsx
+++ components/CountryModal.tsx
@@ -4,16 +4,16 @@ import { BsArrowLeft } from "react-icons/bs";
 
 const CountryModal = ({ country, onClose }: any) => {
   return (
-    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center  p-20">
+    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center p-20">
       <div className="max-w-84 z-40 rounded-lg bg-black bg-opacity-80 p-5">
         <button
           onClick={onClose}
-          className="my-2 mb-6 flex h-8 w-20 items-center justify-center gap-2 text-sm shadow-xl"
+          className="my-2 mb-6 flex h-8 w-20 items-center justify-center gap-2 text-sm shadow-xl md:mb-2"
         >
           <BsArrowLeft />
           Back
         </button>
-        <div className="z-40 flex flex-col items-center gap-5 lg:flex-row lg:gap-52">
+        <div className="z-40 flex flex-col items-center gap-5 lg:flex-row lg:gap-12">
           <Image
             src={country.flags.png}
             alt={`Flag of ${country.name.common}`}
@@ -31,7 +31,7 @@ const CountryModal = ({ country, onClose }: any) => {
                 Languages:
                 {Object.values(country.languages).map(
                   (languages, index, array) => (
-                    <span key={country.name}>
+                    <span key={index}>
                       {` ${languages}`}
                       {index < array.length - 1 ? ", " : ""}
                     </span>
